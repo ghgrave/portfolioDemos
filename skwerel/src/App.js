7c1cabfd566b4df5161fb5afd311b6c3c9788eb5
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-03 18:30:59 -0600

    updated background style

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -3,11 +3,12 @@ import Background from '../src/images/skwerel-logo-white-1280.png';
 import './App.css';
 
 let styles = {
-  height: '94vh',
-  width: `97%`,
-  background: `#02342a url(${Background}) center center no-repeat`,
-  margin: `0 auto`,
-  border: '1.5em ridge #551717'
+  display: `flex`,
+  flexDirection: `column`,
+  height: '100vh',
+  width: `100%`,
+  background: `#02342a url(${Background}) center no-repeat`,
+  margin: `0 auto`
 }
 
 function App() {
