86f3bbd0f88f3191ec72ba8391498e7ca153a84d
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-03 18:24:45 -0600

    changed file structure and added backgroundimage

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Background from '../src/images/skwerel-logo-white-1280.png';
 import './App.css';
+
+let styles = {
+  height: '94vh',
+  width: `97%`,
+  background: `#02342a url(${Background}) center center no-repeat`,
+  margin: `0 auto`,
+  border: '1.5em ridge #551717'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app_container" style={styles}>
+      
     </div>
   );
 }
