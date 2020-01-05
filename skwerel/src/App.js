897bf84846f0c5ebcfe9a26bb282c95c1d11df1f
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-04 19:28:33 -0600

    worked on logo and form styling

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -4,12 +4,18 @@ import {background_image} from './helpers';
 import Background from '../src/images/skwerel-logo-white-1280.png';
 import './App.css';
 
-background_image.background = `#02342a url(${Background}) center no-repeat`;
+background_image.background = `#372449 url(${Background}) center no-repeat`;
 
 function App() {
   return (
     <div id="app_container" style={background_image}>
-      
+      <h1>SKWEREL</h1>
+      <form id='loginForm' action="">
+        <label htmlFor="username"></label>
+        <input type="text" name="username" id="username" placeholder='username' autoComplete='off'/>
+        <label htmlFor="password"></label>
+        <input type="password" name="password" id="password" placeholder='password'/>
+      </form>
     </div>
   );
 }
--- skwerel/src/helpers.js
+++ skwerel/src/helpers.js
@@ -2,8 +2,8 @@ export const background_image = {
     display: `flex`,
     flexDirection: `column`,
     height: '100vh',
-    width: `100%`,
-    background: `#02342a`,
+    width: `60%`,
+    background: `#583c09`,
     backgroundSize: `contain`,
     margin: `0 auto`
   }
