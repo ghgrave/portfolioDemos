6207c993dc783f8957b773dc42b0656e0f085831
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-04 18:29:12 -0600

    created helpers.js file for background styling

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
+
+import {background_image} from './helpers';
 import Background from '../src/images/skwerel-logo-white-1280.png';
 import './App.css';
 
-let styles = {
-  display: `flex`,
-  flexDirection: `column`,
-  height: '100vh',
-  width: `100%`,
-  background: `#02342a url(${Background}) center no-repeat`,
-  margin: `0 auto`
-}
+background_image.background = `#02342a url(${Background}) center no-repeat`;
 
 function App() {
   return (
-    <div id="app_container" style={styles}>
+    <div id="app_container" style={background_image}>
       
     </div>
   );
---
+++ skwerel/src/helpers.js
@@ -0,0 +1,9 @@
+export const background_image = {
+    display: `flex`,
+    flexDirection: `column`,
+    height: '100vh',
+    width: `100%`,
+    background: `#02342a`,
+    backgroundSize: `contain`,
+    margin: `0 auto`
+  }
