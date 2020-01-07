b4142a63c72949579130b690b3314526474cb280
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-06 19:23:42 -0600

    updated .gitignore

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -9,7 +9,7 @@ background_image.background = `#372449 url(${Background}) center no-repeat`;
 function App() {
   return (
     <div id="app_container" style={background_image}>
-      <h1>SKWEREL</h1>
+      <h1>skwerel</h1>
       <form id='loginForm' action="">
         <label htmlFor="username"></label>
         <input type="text" name="username" id="username" placeholder='username' autoComplete='off'/>
