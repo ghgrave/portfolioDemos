85ea3d3ac1fd7b55823bf75a3f99f9d3f113edba
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-06 21:30:41 -0600

    added bootstrap and fixed responsiveness for home page

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import {background_image} from './helpers';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Background from '../src/images/skwerel-logo-white-1280.png';
 import './App.css';
 
@@ -8,15 +13,23 @@ background_image.background = `#372449 url(${Background}) center no-repeat`;
 
 function App() {
   return (
-    <div id="app_container" style={background_image}>
-      <h1>skwerel</h1>
-      <form id='loginForm' action="">
-        <label htmlFor="username"></label>
-        <input type="text" name="username" id="username" placeholder='username' autoComplete='off'/>
-        <label htmlFor="password"></label>
-        <input type="password" name="password" id="password" placeholder='password'/>
-      </form>
-    </div>
+    <Container fluid={true}>
+      <Row>
+        <Col lg={3} sm={12} xs={12}>
+          <form id='loginForm' action="">
+            <label htmlFor="username"></label>
+            <input type="text" name="username" id="username" placeholder='username' autoComplete='off'/>
+            <label htmlFor="password"></label>
+            <input type="password" name="password" id="password" placeholder='password'/>
+          </form>
+        </Col>
+        <Col lg={6} sm={12} xs={12} style={background_image}>
+          <h1>skwerel</h1>
+        </Col>
+        <Col lg={3} sm={12} xs={12}></Col>
+      </Row>
+
+    </Container>
   );
 }
 
--- skwerel/src/helpers.js
+++ skwerel/src/helpers.js
@@ -2,7 +2,7 @@ export const background_image = {
     display: `flex`,
     flexDirection: `column`,
     height: '100vh',
-    width: `60%`,
+    width: `100%`,
     background: `#583c09`,
     backgroundSize: `contain`,
     margin: `0 auto`
