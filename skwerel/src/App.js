c084684bb6e3974fee906bdedd91945ce260b9ef
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-08 20:51:29 -0600

    fixed box shadow and added customized image

--- skwerel/src/App.js
+++ skwerel/src/App.js
@@ -6,19 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Background from '../src/images/skwerel-logo-white-1280.png';
-import Background2 from '../src/images/skwerel-logo-640.png';
+import Background from '../src/images/skwerel-logo-1280-shadow.svg';
 import './App.css';
 
-background_image.background = `#372449 url(${Background}) center no-repeat`;
-background_image2.background = `#372449 url(${Background2}) center no-repeat`;
+background_image.background = `#372449 url(${Background}) center bottom no-repeat`;
 
 
 function App() {
   return (
     <Container fluid={true}>
       <Row>
-        <Col lg={3} sm={12} xs={12}>
+        <Col lg={3} >
           <form id='loginForm' action="">
             <label htmlFor="username"></label>
             <input type="text" name="username" id="username" placeholder='username' autoComplete='off'/>
@@ -26,10 +24,10 @@ function App() {
             <input type="password" name="password" id="password" placeholder='password'/>
           </form>
         </Col>
-        <Col lg={6} sm={12} xs={12} style={background_image}>
+        <Col lg={6} style={background_image}>
             <h1>skwerel</h1>
         </Col>
-        <Col lg={3} sm={12} xs={12}></Col>
+        <Col lg={3} ></Col>
       </Row>
 
     </Container>
--- skwerel/src/helpers.js
+++ skwerel/src/helpers.js
@@ -6,19 +6,6 @@ export const background_image = {
     background: `#583c09`,
     backgroundSize: `contain`,
     margin: `0 auto`,
-    position: "relative",
-    zIndex: 2,
-    filter: "drop-shadow(5px 5px 5px #222)"
+    boxShadow: `0px -1px 16px 12px rgba(0,0,0,0.75)`
+    // filter: "drop-shadow(5px 5px 5px #222)"
   }
-
-export const background_image2 = {
-  display: `flex`,
-  flexDirection: `column`,
-  height: '100vh',
-  width: `100%`,
-  background: `#583c09`,
-  backgroundSize: `contain`,
-  margin: `0 auto`,
-  position: "relative",
-  zIndex: 3
-}
