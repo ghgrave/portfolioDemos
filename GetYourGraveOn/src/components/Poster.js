f4ea32bdd7008672d09aedc83de752eed8d45ed6
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-01-31 17:54:16 -0600

    updated path for missing poster and added title attributes

--- GetYourGraveOn/src/components/Poster.js
+++ GetYourGraveOn/src/components/Poster.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './Poster.css'
 
+import werewolfCartoon from '../assets/images/werewolfCartoon.jpg'
+
 function Poster (props) {
     let displayPoster;
 
     let { poster_src, poster_path, id, title, genres, release_date, overview, imdb_id} = props.movies;
-    poster_src = poster_path ? `https://image.tmdb.org/t/p/w200${poster_path}` : 'assets/werewolf-collection-2-002.jpg'
+    poster_src = poster_path ? `https://image.tmdb.org/t/p/w200${poster_path}` : werewolfCartoon;
     if(props.posterOnly) {    
         displayPoster = <div className='test'>
-                            <img alt="poster" src={poster_src} onClick={props.onClick} key={id} movieid={id}></img>
+                            <img alt={title} title={title} src={poster_src} onClick={props.onClick} key={id} movieid={id}></img>
                         </div>
     } else {
         let imdb_idLink = `https://www.imdb.com/title/${imdb_id}`
         displayPoster =  <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
                             <div className="flipper">
                                 <div className="front">
-                                    <img alt="poster" src={poster_src} key={id} ></img>
+                                    <img alt={title} title={title} src={poster_src} key={id} ></img>
                                 </div>
 
                                 {/* sets back of flipper card to specific data based on results.Title */}
