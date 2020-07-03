0c0bb0c4c7f635a5e4e4067cadf3dad9a3d825fe
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-07-03 15:24:18 -0500

    built dummy calls for home page movie and tv displays

--- skwerelReact/client/src/components/Home/Home.js
+++ skwerelReact/client/src/components/Home/Home.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import DATA from "../../config/config";
+// import MOVIE_END from "../../config/config";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -13,10 +16,48 @@ import "./Home.css";
 // background_image.background = `#372449 url(${Background}) center bottom no-repeat`;
 
 function Home(props) {
+
+  const [motd, setMotd] = useState({});
+  const [tvotd, setTvotd] = useState({});
+
+  function randMovie() {
+    axios
+      .get(`${DATA.MOVIE_URL}/now_playing?api_key=${DATA.CONFIG.TMDB_KEY}`)
+      .then((movieResults) => {
+        let movie =
+          movieResults.data.results[
+            Math.floor(Math.random() * movieResults.data.results.length)
+          ];
+          let poster = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
+        setMotd({...movie, poster});
+      })
+      .catch((err) => console.log("Error getting MOTD: ", err));
+  }
+
+  function randTv() {
+    axios
+      .get(`${DATA.TV_URL}/popular?api_key=${DATA.CONFIG.TMDB_KEY}`)
+      .then((tvResults) => {
+        let tv =
+          tvResults.data.results[
+            Math.floor(Math.random() * tvResults.data.results.length)
+          ];
+          let poster = `https://image.tmdb.org/t/p/w200${tv.poster_path}`
+        setTvotd({...tv, poster});
+      })
+      .catch((err) => console.log("Error getting TVOTD: ", err));
+  }
+
+  useEffect(() => {
+    randMovie();
+    randTv()
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     props.history.push("/user");
   };
+  
   return (
     <Container fluid={true} id="home_container">
       <Row>
@@ -43,7 +84,7 @@ function Home(props) {
           </form>
         </Col>
         <Col
-        className='mt-2'
+          className="mt-2"
           lg={6}
           style={{
             background_image,
@@ -56,10 +97,14 @@ function Home(props) {
           <h1>skwerel</h1>
         </Col>
         <Col lg={3}>
-          <div className="mediaBox"></div>
-          <h3>Movie of the Day</h3>
-          <div className="mediaBox"></div>
-          <h3>TV Show of the Day</h3>
+          <div className="mediaBox">
+            <img src={motd.poster} alt={motd.title} />
+            </div>
+          <h3>Movie</h3>
+          <div className="mediaBox">
+          <img src={tvotd.poster} alt={tvotd.name} />
+          </div>
+          <h3>TV Show</h3>
         </Col>
       </Row>
     </Container>
