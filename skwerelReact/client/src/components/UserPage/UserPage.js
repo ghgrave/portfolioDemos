720222802c85e3da5969ea150d320b67b41ba43d
Jon-Mikel <garrisonhgrave@yahoo.com>
2020-07-04 16:30:19 -0500

    built newsfeed data for diaply on userpage

--- skwerelReact/client/src/components/UserPage/UserPage.js
+++ skwerelReact/client/src/components/UserPage/UserPage.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from "react";
-import CalendarComp from "../Calendar/Calendar"
+import React, { useState, useEffect } from "react";
+import CalendarComp from "../Calendar/Calendar";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-import DATA from '../../utils/data/fakeData'
+import DATA from "../../utils/data/fakeData";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -13,14 +14,30 @@ import "./UserPage.css";
 background_image.background = `initial`;
 
 function UserPage() {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+  const [newsfeed, setNewsfeed] = useState([]);
 
-  useEffect(()=> {
-    console.log(DATA.data)
-    setEvents(DATA.data)
-  },[])
+  useEffect(() => {
+    console.log(DATA.data);
+    setEvents(DATA.data);
+  }, []);
 
-
+  useEffect(() => {
+    axios
+      .get("/user/newsfeed")
+      .then((response) => {
+       
+        const displayNewsfeed = response.data.map((data, index) => {
+          return (
+            <div key={index} className="newsBox">
+              <a  href={data.link} target='_blank'>{data.title}</a>
+            </div>
+          );
+        });
+        setNewsfeed(displayNewsfeed) 
+      })
+      .catch();
+  }, []);
 
   return (
     <Container fluid={true} id="userPage_container">
@@ -28,55 +45,14 @@ function UserPage() {
         <Col lg={3}>
           <h3>Newsfeed</h3>
           <div id="media_container">
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
-            <div className="newsBox">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              cupiditate, perspiciatis id perferendis ipsam dolor excepturi
-              nostrum animi delectus cum modi inventore laudantium culpa quaerat
-              quis ex alias beatae molestias?
-            </div>
+            {newsfeed}
           </div>
         </Col>
         <Col lg={6} style={background_image}>
           <h1>gHgrave</h1>
           <div id="calendar_container">
             <div id="calendar">
-              <CalendarComp events={events}/>
+              <CalendarComp events={events} />
             </div>
           </div>
           {/* <h4 className="mt-1">Upcoming:</h4>
