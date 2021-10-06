7fddff0e0588dfcaa25e3d7f2bed118b32026903
Jon-Mikel <garrisonhgrave@yahoo.com>
2021-10-05 20:12:55 -0500

    added navbar and home comps

---
+++ monster-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Monster Blog</h1>
+      <div className="links">
+        <a href="/">Lair</a>
+        <a href="/create">New Creation</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
